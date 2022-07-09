c9c21d13e41ac17f023feae884ef2e62e240cbc5
Matthew <mwhiteman83@gmail.com>
2022-07-08 20:37:32 -0600

    added server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const { Console } = require('console');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
