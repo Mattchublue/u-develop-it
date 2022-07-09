8726507eca181e1027ea77281cb90a5126002529
Matthew <mwhiteman83@gmail.com>
2022-07-08 21:12:58 -0600

    added array

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const { Console } = require('console');
 const express = require('express');
+const mysql =require('mysql2');
+
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+})
+
 app.use((req, res) => {
     res.status(404).end();
-});
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
 });
 
 app.listen(PORT, () => {
